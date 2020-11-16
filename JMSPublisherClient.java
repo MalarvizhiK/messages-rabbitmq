be18bde5e63853a5479dfe4a4a915316c0f82251
MalarvizhiK <k.malarvizhi@in.ibm.com>
2020-11-16 16:26:35 +0530

    Add files via upload

--- JMSPublisherClient.java
+++ JMSPublisherClient.java
@@ -9,7 +9,7 @@ import javax.jms.Topic;
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory;
 
-public class JMSSubscriberClient {
+public class JMSPublisherClient {
     public static void main(String[] args) throws Exception {
         Connection connection = null;
         try {
@@ -33,23 +33,13 @@ public class JMSSubscriberClient {
                     Session.AUTO_ACKNOWLEDGE);
             Topic topic = session.createTopic("customerTopic");
  
-            // Consumer1 subscribes to customerTopic
-            MessageConsumer consumer1 = session.createDurableSubscriber(topic, "consumer1", "", false);     
+            // Publish
+            String payload = "Task";
+            TextMessage msg = session.createTextMessage(payload);
+            MessageProducer publisher = session.createProducer(topic);
+            System.out.println("Sending text '" + payload + "'");
+            publisher.send(msg, javax.jms.DeliveryMode.PERSISTENT, javax.jms.Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
  
-            // Consumer2 subscribes to customerTopic
-            MessageConsumer consumer2 = session.createDurableSubscriber(topic, "consumer2", "", false); 
- 
-            connection.start();
-             
-            TextMessage msg = (TextMessage) consumer1.receive();
-            System.out.println("Consumer1 receives " + msg.getText());
-            //acknowledge
-            msg.acknowledge(); 
-             
-            msg = (TextMessage) consumer2.receive();
-            System.out.println("Consumer2 receives " + msg.getText());
- 	    //acknowledge
-            msg.acknowledge();	
             session.close();
         } finally {
             if (connection != null) {
